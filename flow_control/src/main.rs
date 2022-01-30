d3587d1f8f98deebc75e32f73f430bb224de71e4
haruhiko28 <not.hisano.soccer@gmail.com>
2022-01-31 00:32:05 +0900

    add flow_controll

---
+++ flow_control/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let _number = 3;
+    let _number = 7;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    if number % 2 == 0 {
+        println!("number is divisible by two");
+    } else if number % 3 == 0 {
+        println!("number is divisible by three");
+    } else {
+        println!("number is not divisible by two or three");
+    }
+    
+}
