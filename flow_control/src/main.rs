b43ff2184893e17bcb491416ef4c2cd9815ac96b
haruhiko28 <not.hisano.soccer@gmail.com>
2022-01-31 21:40:07 +0900

    add section03

--- flow_control/src/main.rs
+++ flow_control/src/main.rs
@@ -25,4 +25,21 @@ fn main() {
         num = num - 1;
     }
     println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index = index + 1;
+    }
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
 }
